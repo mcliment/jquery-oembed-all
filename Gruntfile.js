2e6765bf07b1c63d40c17bef94259f4fcbac153a
mcliment <mcliment@avanzis.com>
2014-12-22 17:54:43 +0100

    Better grunt experience

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,41 @@
+'use strict';
+
 module.exports = function(grunt) {
 
 	grunt.initConfig({
+		// Metadata
+		pkg: grunt.file.readJSON('bower.json'),
+		banner: '/*! jquery-oembed-plugin - v<%= pkg.version %> - <%= grunt.template.today("yyyy-mm-dd") %>\n' +
+		  ' *\n' +
+		  ' * Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.authors.join(", ") %>\n' +
+		  ' * Licensed under the MIT license\n' +
+		  ' *\n' +
+		  ' * Forked by Andrew Mee to Provide a slightly different kind of embedding experience\n' +
+		  ' */\n',
+
+		concat: {
+			options: {
+				banner: '<%= banner %>',
+				stripBanners: true,
+				sourceMap: true
+			},
+			dist: {
+				src: ['src/scripts/jquery.oembed.js','src/scripts/md5.js'],
+				dest: 'dist/jquery.oembed.js'
+			}
+		},
+		uglify: {
+			options: {
+				banner: '<%= banner %>'
+			},
+			dist: {
+				src: '<%= concat.dist.dest %>',
+				dest: 'dist/jquery.oembed.min.js'
+			}
+    	},
+    	qunit: {
+      		files: ['test/**/*.html']
+    	},
 		jshint: {
 			gruntfile: {
         		options: {
@@ -21,16 +56,7 @@ module.exports = function(grunt) {
         		},
         		src: ['test/**/*.js']
       		}
-		},
-		qunit: {
-      		files: ['test/**/*.html']
-    	},
-    	uglify: {
-			release: {
-				src: 'src/scripts/jquery.oembed.js',
-				dest: 'dist/jquery.oembed.min.js'
-			}
-    	},
+		},		
 		sass: {
 			debug: {
 				options: {
@@ -41,7 +67,7 @@ module.exports = function(grunt) {
 					'dist/jquery.oembed.css': 'src/styles/jquery.oembed.scss'
 				}
 			},
-			release: {
+			dist: {
 				options: {
 					style: 'compressed',
 					sourcemap: 'none'
@@ -63,11 +89,12 @@ module.exports = function(grunt) {
 		}
 	});
 
+	grunt.loadNpmTasks('grunt-contrib-concat');
 	grunt.loadNpmTasks('grunt-contrib-jshint');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-qunit');
 	grunt.loadNpmTasks('grunt-contrib-sass');
 	grunt.loadNpmTasks('grunt-scss-lint');
 
-	grunt.registerTask('default', ['jshint', 'scsslint', 'sass']);
+	grunt.registerTask('default', ['jshint','qunit','concat','uglify','scsslint', 'sass']);
 };
